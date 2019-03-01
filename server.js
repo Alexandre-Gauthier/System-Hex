e7da92403e9d615f172ba16c029a017758d4bc3e
Alexandre Gauthier <alexandre-gauthier@hotmail.com>
2019-03-01 00:50:56 -0500

    Server.js mod

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const basicAuth = require('./_helpers/basic-auth');
 const errorHandler = require('./_helpers/error-handler');
 
+const MongoClient = require('mongodb').MongoClient
+const config = require('./config');
+let db = null;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,3 +22,24 @@ app.use('/users', require('./users/users.controller'));
 
 // global error handler
 app.use(errorHandler);
+
+
+// start server
+const port = 3000;
+MongoClient.connect(config, (err, client) => {
+	if (err) return console.log(err)
+	db = client.db('system-hex') // whatever your database name is
+	if(db!= null){
+		const server = app.listen(port, function () {
+			console.log('Server listening on port ' + port);
+		});
+	}
+})
+
+app.get('/server',(req,res)=>{
+	console.log(req.user.id);
+	db.collection('quotes').find().toArray((err, result) => {
+		if (err) return console.log(err)
+		res.send(result);
+	});
+});
