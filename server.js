b4a82fa2257cc7eac23b17fc8b5b3d8556ac4861
Alexandre Gauthier <alexandre-gauthier@hotmail.com>
2019-03-01 01:27:50 -0500

    Server

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+
 // use basic HTTP auth to secure the api
 app.use(basicAuth);
 
@@ -32,4 +33,5 @@ const server = app.listen(port, function () {
 
 app.get('/server',(req,res)=>{
 	console.log('Hello there')
+	res.send('Hello there')
 });
