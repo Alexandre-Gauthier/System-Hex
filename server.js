4bd0f88857e8d3d1829141e1c8aeba9f414e51d4
Alexandre Gauthier <alexandre-gauthier@hotmail.com>
2019-03-01 21:54:57 -0500

    Server-side page

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
+const userService = require('../users/user.service');
+
+/*
 const basicAuth = require('./_helpers/basic-auth');
-const errorHandler = require('./_helpers/error-handler');
+const errorHandler = require('./_helpers/error-handler');*/
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
@@ -39,11 +42,12 @@ var initDb = function(callback) {
 		dbDetails.type = 'MongoDB';
 
 		console.log('Connected to MongoDB at: %s', mongoURL);
-
+/*
 		db.collection('users').find().toArray((err, result) => {
 			if (err) return console.log(err)
 			users = result;
 		});
+
 
 		// use basic HTTP auth to secure the api
 		app.use(basicAuth);
@@ -52,7 +56,7 @@ var initDb = function(callback) {
 		app.use('/users', require('./users/users.controller'));
 
 		// global error handler
-		app.use(errorHandler);
+		app.use(errorHandler);*/
 
 		// start server
 		const server = app.listen(port, function () {
@@ -64,6 +68,7 @@ var initDb = function(callback) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
 app.use(cookieParser());
 
 app.use(session({
@@ -83,7 +88,7 @@ app.use((req, res, next) => {
     next();
 });
 
-let sessionChecker = (req, res, next) => {
+var sessionChecker = (req, res, next) => {
     if (req.session.user && req.cookies.user_sid) {
         res.redirect('/dashboard');
     } else {
@@ -91,22 +96,42 @@ let sessionChecker = (req, res, next) => {
     }
 };
 
-// route for user logout
-app.get('/logout', (req, res) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.clearCookie('user_sid');
-        res.redirect('/');
-    } else {
-        res.redirect('/login');
-    }
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
 });
 
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
+	})
+
+    .post((req, res) => {
+        var username = req.body.username,
+			password = req.body.password;
+
+		const user = await userService.authenticate({ username, password });
+		if (!user) {
+			res.redirect('/login');
+		}else{
+			req.session.user = user.dataValues;
+			res.redirect('/observation');
+		}
+    });
+
+app.get('/observation', (req, res) => {
+	if (req.session.user && req.cookies.user_sid) {
+		res.sendFile(__dirname + '/public/observation.html');
+	} else {
+		res.redirect('/login');
+	}
+});
+/*
 app.get('/login',(req,res)=>{
 	db.collection('quotes').find().toArray((err, result) => {
 		if (err) return console.log(err)
 		res.send(result);
 	});
-});
+});*/
 
 initDb(function(err){
 	console.log('Error connecting to Mongo. Message:\n'+err);
