f79b39032a42d9f0493a43928c1b7a089edf5d9e
Alexandre Gauthier <alexandre-gauthier@hotmail.com>
2019-03-04 18:26:22 -0500

    Test var session by rand

--- models/basic-auth.js
+++ models/basic-auth.js
@@ -9,6 +9,7 @@ async function auth(username,password){
 async function authenticate({ username, password }) {
     let users = getUsers();
     const user = users.find(u => u.username === username && u.password === password);
+    console.log(user)
     if (user) {
         const { password, ...userWithoutPassword } = user;
         return userWithoutPassword;
