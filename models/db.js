b0daf328377c996981b0aa2d68da9a38ebb33bcb
Alexandre Gauthier <alexandre-gauthier@hotmail.com>
2019-03-26 17:26:12 -0400

    Change DB for class

--- models/db.js
+++ models/db.js
@@ -1,27 +1,36 @@
 const client = require('mongodb').MongoClient;
 const config = require("./config");
 
-let _db;
 
 module.exports = class MongoDB{
-	constructor(){
-		this._db;
+	static connectMongo(){
+		if ( this._db ) return Promise.resolve(this._db)
+        return client.connect(config.connectionString)
+            .then(db => this._db = db)
 	}
 
 	initDb(callback){
 		if(this._db){
 			console.warn("Trying to init DB again!");
-			return callback(null, this._db);
+			return callback(null,this._db);
 		}
 
-		client.connect(config.connectionString, function(err, conn) {
-			if (err) {return callback(err);}
+		client.connect(config.connectionString)
+            .then(db => {
+				this._db = db.db('system-hex');
+				console.log('Connected to MongoDB at: %s', config.connectionString);
+				return callback(null,this._db);
+			})
 
-			this._db = conn.db('system-hex');
+		// client.connect(config.connectionString, function(err, conn) {
+		// 	if (err) {return callback(err);}
 
-			console.log('Connected to MongoDB at: %s', config.connectionString);
-			return callback(null,this._db);
-		});
+
+		// 	this._db = conn.db('system-hex');
+
+		// 	console.log('Connected to MongoDB at: %s', config.connectionString);
+		// 	return callback(null,this._db);
+		// });
 	}
 
 	getDb(){
@@ -60,60 +69,5 @@ module.exports = class MongoDB{
 				callback(response);
 			});
 		}
-	}
-}
-
-function initDb(callback) {
-	if(_db){
-		console.warn("Trying to init DB again!");
-        return callback(null, _db);
-    }
-
-	client.connect(config.connectionString, function(err, conn) {
-		if (err) {return callback(err);}
-
-		_db = conn.db('system-hex');
-
-		console.log('Connected to MongoDB at: %s', config.connectionString);
-		return callback(null,_db);
-	});
-
-};
-
-function getDb(){
-    assert.ok(_db, "Db has not been initialized. Please called init first.");
-    return _db;
-}
-
-function getUsers(callback){
-	if(_db){
-		_db.collection('users').find().toArray((err, result) => {
-			if (err) return console.log(err)
-			callback(result)
-		});
-	}
-}
-
-function getSystems(userID,callback){
-	if(_db){
-		_db.collection('systems').find({userId: userID},{systems:1}).toArray((err, result) => {
-			if (err) return console.log(err);
-			callback(result[0]);
-		});
-	}
-}
-
-function getSystem(userID,systemID,callback){
-	if(_db){
-		_db.collection('systems').find({userId: userID},{systems:1}).toArray((err, result) => {
-			if (err) return console.log(err);
-			let response = null;
-			result[0].systems.forEach(system => {
-				if(system.id == systemID){
-					response = system;
-				}
-			});
-			callback(response);
-		});
 	}
 }
